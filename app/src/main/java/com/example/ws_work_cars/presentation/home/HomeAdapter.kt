9b8fd7880b245bfc0a7f09285d8746e10bd4c031
gabrielbmmaia <gbmmaia.contato@gmail.com>
2022-11-02 16:41:25 -0300

    feat (home_adapter): criação do adapter do recyclerview do fragment Home

---
+++ app/src/main/java/com/example/ws_work_cars/presentation/home/HomeAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.ws_work_cars.presentation.home
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+import com.example.ws_work_cars.databinding.HomeRecyclerviewCarBinding
+import com.example.ws_work_cars.domain.model.Car
+
+class HomeAdapter(
+    private val onItemClicked: (Car) -> Unit
+) : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+
+    private var carList = mutableListOf<Car>()
+
+
+    inner class HomeViewHolder(private val binding: HomeRecyclerviewCarBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        private val modeloCarro = binding.modeloCarro
+        private val marcaCarro = binding.marcaCarro
+        private val corCarro = binding.corCarro
+        private val portasCarro = binding.portasCarro
+        private val combustivelCarro = binding.combustivelCarro
+        private val anoCarro = binding.anoCarro
+        private val valorCarro = binding.valorCarro
+
+        fun bindView(car: Car, onItemClicked: (Car) -> Unit) {
+            modeloCarro.text = car.nomeModelo
+            marcaCarro.text = car.marcaNome
+            corCarro.text = car.cor
+            portasCarro.text = car.numPortas
+            combustivelCarro.text = car.combustivel
+            anoCarro.text = car.ano
+            valorCarro.text = car.valorFipe
+
+            itemView.setOnClickListener {
+                onItemClicked(car)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = HomeRecyclerviewCarBinding.inflate(inflater, parent, false)
+
+        return HomeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        holder.bindView(carList[position], onItemClicked)
+    }
+
+    override fun getItemCount(): Int = carList.size
+}
