429940d5fcb592851dfc6b6021b0d80e6cc81cb6
gabrielbmmaia <gbmmaia.contato@gmail.com>
2022-11-02 17:45:56 -0300

    feat (home_viewmodel): criação do viewmodel de controle da HomeFragmentView

--- app/src/main/java/com/example/ws_work_cars/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/ws_work_cars/presentation/home/HomeViewModel.kt
@@ -1,12 +1,57 @@
 package com.example.ws_work_cars.presentation.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ws_work_cars.core.Resource
 import com.example.ws_work_cars.domain.use_cases.getCarListUseCase
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class HomeViewModel(
-    private val carList : getCarListUseCase
+    private val carListUseCase: getCarListUseCase
 ) : ViewModel() {
 
+    /**
+     * Variáveis observáveis
+     * */
 
+    private val _carListState = MutableStateFlow<CarListState>(CarListState.Empity)
+    val carListState: StateFlow<CarListState> get() = _carListState
 
+    init {
+        getCarList()
+    }
+
+    /**
+     * Função para controlar os tipos de estados da variável observável
+     * */
+
+    private fun getCarList() {
+
+        viewModelScope.launch(IO) {
+
+            carListUseCase().collect { result ->
+
+                when (result) {
+
+                    is Resource.Error -> {
+                        _carListState.value =
+                            CarListState.Error(result.messager)
+                    }
+
+                    is Resource.Success -> {
+                        _carListState.value =
+                            CarListState.Success(result.data)
+                    }
+
+                    Resource.Loading -> {
+                        _carListState.value =
+                            CarListState.Loading
+                    }
+                }
+            }
+        }
+    }
 }
