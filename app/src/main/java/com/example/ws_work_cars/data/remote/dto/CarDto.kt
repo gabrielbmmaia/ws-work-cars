6b0fe4eda54a5770b23a53c385c9263f41017230
gabrielbmmaia <gbmmaia.contato@gmail.com>
2022-11-02 16:01:27 -0300

    fix (data_carDto): tratamento de possíveis nulos da API

--- app/src/main/java/com/example/ws_work_cars/data/remote/dto/CarDto.kt
+++ app/src/main/java/com/example/ws_work_cars/data/remote/dto/CarDto.kt
@@ -4,35 +4,35 @@ import com.example.ws_work_cars.domain.model.Car
 import com.squareup.moshi.Json
 
 data class CarDto(
-    val ano: String = "",
-    val combustivel: String = "",
-    val cor: String = "",
-    val id: Long = 0,
+    val ano: String?,
+    val combustivel: String?,
+    val cor: String?,
+    val id: Long,
     @Json(name = "marca_id")
-    val marcaId: Int = 0,
+    val marcaId: Int,
     @Json(name = "marca_nome")
-    val marcaNome: String = "",
+    val marcaNome: String?,
     @Json(name = "nome_modelo")
-    val nomeModelo: String = "",
+    val nomeModelo: String?,
     @Json(name = "num_portas")
-    val numPortas: String = "",
+    val numPortas: String?,
     @Json(name = "timestamp_cadastro")
-    val timestampCadastro: String = "",
+    val timestampCadastro: String,
     @Json(name = "valor_fipe")
-    val valorFipe: String = ""
+    val valorFipe: String?
 ) {
 
     fun toCar(): Car {
 
         return Car(
-            ano = ano,
-            combustivel = combustivel,
-            cor = cor,
+            ano = ano ?: "",
+            combustivel = combustivel ?: "",
+            cor = cor ?: "",
             id = id,
-            marcaNome = marcaNome,
-            nomeModelo = nomeModelo,
-            numPortas = numPortas,
-            valorFipe = valorFipe
+            marcaNome = marcaNome ?: "",
+            nomeModelo = nomeModelo ?: "",
+            numPortas = numPortas ?: "",
+            valorFipe = valorFipe ?: ""
         )
     }
 }
