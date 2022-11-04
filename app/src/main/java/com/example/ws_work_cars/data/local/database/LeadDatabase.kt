a61edafcb8eb8dd2470799deaf85e703ced53aed
gabrielbmmaia <gbmmaia.contato@gmail.com>
2022-11-04 12:15:11 -0300

    feat (data_local): criação da instancia de database do Room

---
+++ app/src/main/java/com/example/ws_work_cars/data/local/database/LeadDatabase.kt
@@ -0,0 +1,48 @@
+package com.example.ws_work_cars.data.local.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ws_work_cars.data.local.dao.LeadDao
+import com.example.ws_work_cars.data.local.entities.LeadDb
+
+
+@Database(
+    entities = [LeadDb::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class LeadDatabase : RoomDatabase(){
+
+    abstract val dao: LeadDao
+
+    /**
+     * Aqui estamos criando uma instancia do database caso ela ainda não exista
+     * e caso exista estamos pegando a mesma referência
+     * */
+    companion object{
+
+        @Volatile
+        private var INSTANCE: LeadDatabase? = null
+
+        fun getInstance(context: Context) : LeadDatabase{
+
+            synchronized(this){
+
+                var instance = INSTANCE
+
+                if(instance == null) {
+
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        LeadDatabase::class.java,
+                        "leads_db"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return  instance
+            }
+        }
+    }
+}
